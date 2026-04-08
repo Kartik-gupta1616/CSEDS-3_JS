9b59f6b350649236eb53152e3015bea11360ed50
Kartik_2025B15410166 <kg3006609@gmail.com>
2026-04-08 15:11:34 +0530

    Delete firstprogram.js

--- firstprogram.js
+++
@@ -1,9 +0,0 @@
-var msg="ABES EC";
-        document.write(msg);
-        console.log(msg);
-
-        function hello(){
-            var msg2="Thanks to Visit!"
-            console.log(msg2);
-        }
-        hello();
