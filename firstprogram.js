acc3c581993b26a82e3c48203b216a41b743cf52
Kartik2025B15410166 <kg3006609@gmail.com>
2026-04-08 15:05:24 +0530

    first commit

---
+++ firstprogram.js
@@ -0,0 +1,9 @@
+var msg="ABES EC";
+        document.write(msg);
+        console.log(msg);
+
+        function hello(){
+            var msg2="Thanks to Visit!"
+            console.log(msg2);
+        }
+        hello();
